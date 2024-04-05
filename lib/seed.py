9f64ed1f11fac37e7dc85378bcc7652ec47501c5
toxicanonymous253 <ngugifrancis930@gmail.com>
2024-04-05 16:45:30 +0300

    done

--- lib/seed.py
+++ lib/seed.py
@@ -17,17 +17,43 @@ if __name__ == '__main__':
     session.query(Game).delete()
     session.commit()
 
-# Add a console message so we can see output when the seed file runs
-print("Seeding games...")
+    botw = Game(title="Breath of the wild", platform="Switch", genre="Adventure", price = 60)
+    ffvi = Game(title="Final Fantasy VII", platform="Playstation", genre="RPG", price=30)
+    mk8 = Game(title="Mario Kart 8", platform="Switch", genre="Racing", price=50)
+    css = Game(title="Candy Crush Saga", platform="Switch", genre="Puzzle", price=0)
 
-games = [
-    Game(
-        title=fake.name(),
-        genre=fake.word(),
-        platform=fake.word(),
-        price=random.randint(0, 60)
-    )
-for i in range(50)]
+    session.bulk_save_objects([botw, ffvi, mk8])
+    session.commit()
+    print(session.query(Game).count())
+    print(session.query(Game)[-1])
+    
+    session.query(Game).delete()
+    session.commit()
 
-session.bulk_save_objects(games)
-session.commit()
+    session.bulk_save_objects([botw, ffvi, mk8, css])
+    print(session.query(Game).count())
+    print(session.query(Game)[-1])
+
+    session.query(Game).delete()
+    session.commit()
+
+    # Add a console message so we can see output when the seed file runs
+    print("Seeding games...")
+
+    games = [
+        Game(
+            title=fake.name(),
+            genre=fake.word(),
+            platform=fake.word(),
+            price=random.randint(0, 60)
+        )
+    for i in range(50)]
+
+
+    session.bulk_save_objects(games)
+
+    print(session.query(Game).count())
+    print(session.query(Game).first())
+    print(session.query(Game)[-1])
+
+    session.commit()
